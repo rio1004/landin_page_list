718640eee85ed34ccfb1f4b0e4ea2882a780feb5
iyo <>
2024-09-12 09:53:24 +0800

    disable type checking

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true, // This will disable type checking during the build process
+  },
+};
 
 export default nextConfig;
