b8b8ca19342fe457fdee50cffe9458a963033570
iyo <>
2024-10-03 15:08:05 +0800

    adjust the height of the title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Tabs from "../components/tabs";
 import React from "react";
 export default function Home() {
   return (
-    <div className="lg:p-14 md:p-0 ">
+    <div className="lg:p-14 lg:pt-5 md:p-0 ">
       <div className="Title flex justify-center mt-10 md:mt-0">
         <h1 className="text-4xl font-black">落地页模版演示</h1>
       </div>
